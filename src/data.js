bcbb38c84f86fcd01f8b4738b62b7645dc6d4f0a
Marving Moreton <moreton.marving@gmail.com>
2022-09-11 17:04:58 -0400

    passed objects as props with dummy data

--- src/data.js
+++ src/data.js
@@ -7,5 +7,23 @@ export default [
       endDate: "24 Jan, 2021",
       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
       imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    title: "Le Diamant",
+    country: "Martinique",
+    googleMapsUrl: "https://goo.gl/maps/cBsiwwACHNm9PEJt7",
+    startDate: "8 Nov 2021",
+    endDate: "12 Nov 2021",
+    description: "The town of Le Diamant is located in the commune of Le Diamant in the southwest of Martinique. In front of the village is the famous Diamant rock, a former pirate shelter and fortified point during the fighting between the French and the English in the Caribbean. The town is crossed by the road to Les Anses.",
+    imageUrl: "./public/assets/teddy-charti-diamant.jpg"
+  },
+  {
+    title: "Schoelcher",
+    country: "Martinique",
+    googleMapsUrl: "https://goo.gl/maps/f4pe2Uj1WuJhvFkm7",
+    startDate: "10 January 2022",
+    endDate: "20 January 222",
+    description: "It is thought that Martinique is a corruption of the Taíno name for the island (Madiana/Madinina, meaning 'island of flowers', or Matinino, 'island of women'), as relayed to Christopher Columbus when he visited the island in 1502.[11] According to historian Sydney Daney, the island was called Jouanacaëra or Wanakaera by the Caribs, which means 'the island of iguanas'.[12]",
+    imageUrl: "./public/assets/coconut.jpg"
   }
 ]
